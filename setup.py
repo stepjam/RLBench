16a698de77d3d1979b55868fe0742f20bfc949df
Kentaro Wada <www.kentaro.wada@gmail.com>
2020-02-20 19:39:21 +0000

    Include install_requires from requirements.txt

--- setup.py
+++ setup.py
@@ -5,9 +5,19 @@ from distutils.core import setup
 # Y: Minor version (e.g. new tasks, major changes to existing tasks, etc)
 # Z: Patch version (e.g. small changes to tasks, bug fixes, etc)
 
+
+def get_install_requires():
+    install_requires = []
+    with open('requirements.txt') as f:
+        for req in f:
+            install_requires.append(req.strip())
+    return install_requires
+
+
 setup(name='rlbench',
       version='1.0.4',
       description='RLBench',
+      install_requires=get_install_requires(),
       author='Stephen James',
       author_email='slj12@ic.ac.uk',
       url='https://www.doc.ic.ac.uk/~slj12',
