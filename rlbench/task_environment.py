f030a37042c13078a2529ce80ad477cbf306f3d9
stephen <slj12@ic.ac.uk>
2021-05-17 21:26:14 +0100

    Added option for collision checking in ABS_EE_POSE_PLAN_WORLD_FRAME.

--- rlbench/task_environment.py
+++ rlbench/task_environment.py
@@ -136,7 +136,7 @@ class TaskEnvironment(object):
             prev_values = cur_positions
             done = reached or not_moving
 
-    def _path_action(self, action, relative_to=None):
+    def _path_action(self, action, collision_checking=False, relative_to=None):
         self._assert_unit_quaternion(action[3:])
         # Check if the target is in the workspace; if not, then quick reject
         # Only checks position, not rotation
@@ -149,109 +149,25 @@ class TaskEnvironment(object):
         if not valid:
             raise InvalidActionError('Target is outside of workspace.')
 
-        grasped_objects = self._robot.gripper.get_grasped_objects()
-        colliding_shapes = [s for s in self._pyrep.get_objects_in_tree(
-            object_type=ObjectType.SHAPE) if s not in grasped_objects
-                  and 'Panda' not in s.get_name() and s.is_collidable()
-                  and self._robot.arm.check_arm_collision(s)]
-        [s.set_collidable(False) for s in colliding_shapes]
+        colliding_shapes = []
+        if collision_checking:
+            # Disable collision checking for objs we are already colliding with,
+            # otherwise planning will always fail.
+            grasped_objects = self._robot.gripper.get_grasped_objects()
+            colliding_shapes = [s for s in self._pyrep.get_objects_in_tree(
+                object_type=ObjectType.SHAPE) if s not in grasped_objects
+                      and 'Panda' not in s.get_name() and s.is_collidable()
+                      and self._robot.arm.check_arm_collision(s)]
+            [s.set_collidable(False) for s in colliding_shapes]
         try:
             path = self._robot.arm.get_path(
-                action[:3], quaternion=action[3:], ignore_collisions=False,
+                action[:3], quaternion=action[3:],
+                ignore_collisions=not collision_checking,
                 relative_to=relative_to, trials=300, max_configs=50,
                 max_time_ms=20)
             [s.set_collidable(True) for s in colliding_shapes]
             done = False
             observations = []
-            while not done:
-                done = path.step()
-                self._scene.step()
-                if self._enable_path_observations:
-                    observations.append(self._scene.get_observation())
-                success, terminate = self._task.success()
-                # If the task succeeds while traversing path, then break early
-                if success:
-                    break
-            return observations
-        except IKError as e:
-            [s.set_collidable(True) for s in colliding_shapes]
-            raise InvalidActionError('Could not find a path.') from e
-
-    def _path_action_v2(self, action, relative_to=None):
-        self._assert_unit_quaternion(action[3:])
-
-        # Check if the target is in the workspace; if not, then quick reject
-        # Only checks position, not rotation
-        pos_to_check = action[:3]
-        if relative_to is not None:
-            self._scene.target_workspace_check.set_position(
-                pos_to_check, relative_to)
-            pos_to_check = self._scene.target_workspace_check.get_position()
-        valid = self._scene.check_target_in_workspace(pos_to_check)
-        if not valid:
-            raise InvalidActionError('Target is outside of workspace.')
-
-        observations = []
-        MAX_DELTA = 0.01
-        steps = np.ceil(np.linalg.norm(self._robot.arm.get_tip().get_pose()[:3] - action[:3]) / MAX_DELTA).astype(int)
-        path_poses = np.linspace(self._robot.arm.get_tip().get_pose(), action, steps)
-        calculated_joint_positions = []
-        initial_joint_positions = self._robot.arm.get_joint_positions()
-
-        grasped_objects = self._robot.gripper.get_grasped_objects()
-        colliding_shapes = [s for s in self._pyrep.get_objects_in_tree(
-            object_type=ObjectType.SHAPE) if s not in grasped_objects
-                  and 'Panda' not in s.get_name() and s.is_collidable()
-                  and self._robot.arm.check_arm_collision(s)]
-        [s.set_collidable(False) for s in colliding_shapes]
-        try:
-            for p in path_poses[1:]:
-                joint_positions = self._robot.arm.solve_ik_via_jacobian(
-                    p[:3], quaternion=p[3:], relative_to=relative_to)
-                calculated_joint_positions.append(joint_positions)
-                self._robot.arm.set_joint_positions(joint_positions, disable_dynamics=False)
-                colliding = self._robot.arm.check_arm_collision()
-                if colliding:
-                    calculated_joint_positions = []
-                    break
-
-            self._robot.arm.set_joint_positions(initial_joint_positions)
-
-            # Move until reached target joint positions or until we stop moving
-            # (e.g. when we collide wth something)
-            if len(calculated_joint_positions) > 0:
-                for ii, jp in enumerate(calculated_joint_positions):
-                    self._robot.arm.set_joint_target_positions(jp)
-                    done = False
-                    prev_values = None
-                    while not done:
-                        self._scene.step()
-                        if self._enable_path_observations:
-                            observations.append(self._scene.get_observation())
-                        cur_positions = self._robot.arm.get_joint_positions()
-                        # reached = np.allclose(cur_positions, jp, atol=0.01)
-                        atol = 1.0 if ii == len(jp) - 1 else 0.01
-                        reached = np.allclose(cur_positions, jp, atol=atol)
-                        not_moving = False
-                        if prev_values is not None:
-                            not_moving = np.allclose(
-                                cur_positions, prev_values, atol=0.001)
-                        prev_values = cur_positions
-                        done = reached or not_moving
-
-                [s.set_collidable(True) for s in colliding_shapes]
-                return observations
-        except IKError as e:
-            self._robot.arm.set_joint_positions(initial_joint_positions)
-
-        observations = []
-        try:
-            path = self._robot.arm.get_path(
-                action[:3], quaternion=action[3:], ignore_collisions=False,
-                relative_to=relative_to, trials=300, max_configs=50,
-                max_time_ms=20)
-            [s.set_collidable(True) for s in colliding_shapes]
-            done = False
             while not done:
                 done = path.step()
                 self._scene.step()
@@ -341,13 +257,15 @@ class TaskEnvironment(object):
 
             self._assert_action_space(arm_action, (7,))
             self._path_observations = []
-            self._path_observations = self._path_action(list(arm_action))
+            self._path_observations = self._path_action(
+                list(arm_action), collision_checking=False)
 
-        elif self._action_mode.arm == ArmActionMode.ABS_EE_POSE_PLAN_WORLD_FRAME_V2:
+        elif self._action_mode.arm == ArmActionMode.ABS_EE_POSE_PLAN_WORLD_FRAME_WITH_COLLISION_CHECK:
 
             self._assert_action_space(arm_action, (7,))
             self._path_observations = []
-            self._path_observations = self._path_action_v2(list(arm_action))
+            self._path_observations = self._path_action(
+                list(arm_action), collision_checking=True)
 
         elif self._action_mode.arm == ArmActionMode.DELTA_EE_POSE_PLAN_WORLD_FRAME:
 
