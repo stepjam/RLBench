36e332ce26110e21dfbc3d7dce063b982dc528c8
Stephen James <stepjamuk@gmail.com>
2021-10-06 10:53:28 -0700

    Modify television tasks. Removed tv_off in favour of tv_on; tasks were too similar.

--- rlbench/tasks/change_channel.py
+++ rlbench/tasks/change_channel.py
@@ -1,56 +1,62 @@
 from typing import List, Tuple
-from rlbench.backend.task import Task
-from rlbench.backend.conditions import JointCondition, DetectedCondition
-from pyrep.objects.shape import Shape
-from pyrep.objects.joint import Joint
-from pyrep.objects.dummy import Dummy
-from pyrep.objects.proximity_sensor import ProximitySensor
-from rlbench.backend.spawn_boundary import SpawnBoundary
+
 import numpy as np
+from pyrep.objects.dummy import Dummy
+from pyrep.objects.joint import Joint
+from pyrep.objects.proximity_sensor import ProximitySensor
+from pyrep.objects.shape import Shape
+from rlbench.backend.conditions import JointCondition, DetectedCondition
+from rlbench.backend.spawn_boundary import SpawnBoundary
+from rlbench.backend.task import Task
 
 
 class ChangeChannel(Task):
 
     def init_task(self) -> None:
         self._remote = Shape('tv_remote')
+        self.register_graspable_objects([self._remote])
         self._joint_conditions = [
             JointCondition(Joint('target_button_joint1'), 0.003),
             JointCondition(Joint('target_button_joint2'), 0.003)
         ]
-        self._w6 = Dummy('waypoint6')
-        self._w6z = self._w6.get_position()[2]
-        self.register_graspable_objects([self._remote])
+        self._remote_conditions = [
+            DetectedCondition(Dummy('tv_remote_top'),
+                              ProximitySensor('success0')),
+            DetectedCondition(Dummy('tv_remote_bottom'),
+                              ProximitySensor('success1'))
+        ]
         self._spawn_boundary = SpawnBoundary([Shape('spawn_boundary')])
         self._target_buttons = [
-            Shape('target_button_wrap1'), Shape('target_button_wrap2')]
+            Shape('target_button_wrap1'),
+            Shape('target_button_wrap2')]
+        self._w6 = Dummy('waypoint6')
+        self._w6z = self._w6.get_position()[2]
 
     def init_episode(self, index: int) -> List[str]:
-        detectors = [ProximitySensor('success%d' % i) for i in range(2)]
-        self.register_success_conditions([
-            self._joint_conditions[index],
-            DetectedCondition(self._remote, detectors[0]),
-            DetectedCondition(self._remote, detectors[1])
-        ])
-        x, y, _ = self._target_buttons[index % 2].get_position()
+        self.register_success_conditions(
+            [self._joint_conditions[index]] + self._remote_conditions)
+        x, y, _ = self._target_buttons[index % 3].get_position()
         self._w6.set_position([x, y, self._w6z])
         self._spawn_boundary.clear()
         self._spawn_boundary.sample(self._remote)
+
         btn = ['plus', 'minus']
         chnl = ['up', 'minus']
-        return ['turn the channel %s' % chnl[index],
-                'change the television channel %s' % chnl[index],
-                'point the remote at the tv and press the %s button to turn '
-                'the channel %s' % (btn[index], chnl[index]),
-                'using the tv remote, ensure it is facing the television and '
-                'press the %s button to increment the channel %s by one'
-                % (btn[index], chnl[index]),
-                'find the %s button on the remote, rotate the remote such that'
-                ' it is pointed at the tv, then press the button to change '
-                'the channel %s' % (chnl[index], btn[index])]
+        return [
+            'turn the channel %s' % chnl[index - 1],
+            'change the television channel %s' % chnl[index - 1],
+            'point the remote at the tv and press the %s button to turn '
+            'the channel %s' % (btn[index - 1], chnl[index - 1]),
+            'using the tv remote, ensure it is facing the television and '
+            'press the %s button to increment the channel %s by one'
+            % (btn[index - 1], chnl[index - 1]),
+            'find the %s button on the remote, rotate the remote such that'
+            ' it is pointed at the tv, then press the button to change '
+            'the channel %s' % (chnl[index - 1], btn[index - 1])]
 
     def variation_count(self) -> int:
         return 2
 
     def base_rotation_bounds(self) -> Tuple[Tuple[float, float, float],
                                             Tuple[float, float, float]]:
-        return (0.0, 0.0, -0.5*np.pi), (0.0, 0.0, +0.5 * np.pi)
+        return (0.0, 0.0, -np.pi / 2), (0.0, 0.0, np.pi / 2)
--- rlbench/tasks/tv_on.py
+++ rlbench/tasks/tv_on.py
@@ -1,26 +1,33 @@
 from typing import List, Tuple
-from rlbench.backend.task import Task
-from rlbench.backend.conditions import JointCondition
-from pyrep.objects.shape import Shape
-from pyrep.objects.joint import Joint
-from rlbench.backend.spawn_boundary import SpawnBoundary
+
 import numpy as np
+from pyrep.objects.dummy import Dummy
+from pyrep.objects.joint import Joint
+from pyrep.objects.proximity_sensor import ProximitySensor
+from pyrep.objects.shape import Shape
+from rlbench.backend.conditions import JointCondition, DetectedCondition
+from rlbench.backend.spawn_boundary import SpawnBoundary
+from rlbench.backend.task import Task
 
 
 class TvOn(Task):
 
     def init_task(self) -> None:
         self._remote = Shape('tv_remote')
-        self._spawn_boundary = SpawnBoundary([Shape('spawn_boundary')])
         self.register_graspable_objects([self._remote])
         self.register_success_conditions([
-            JointCondition(Joint('target_button_joint0'), 0.003)
-        ])
+            JointCondition(Joint('target_button_joint0'), 0.003),
+            DetectedCondition(Dummy('tv_remote_top'),
+                              ProximitySensor('success0')),
+            DetectedCondition(Dummy('tv_remote_bottom'),
+                              ProximitySensor('success1'))])
+        self._spawn_boundary = SpawnBoundary([Shape('spawn_boundary')])
 
     def init_episode(self, index: int) -> List[str]:
         self._spawn_boundary.clear()
         self._spawn_boundary.sample(self._remote)
-        return ['turn on the TV',
+        return [
+                'turn on the TV',
                 'point the remote control at the television and turn on the '
                 'television',
                 'pick up the remote and rotate it such that the front of the '
@@ -35,5 +42,4 @@ class TvOn(Task):
 
     def base_rotation_bounds(self) -> Tuple[Tuple[float, float, float],
                                             Tuple[float, float, float]]:
-        return (0.0, 0.0, -0.5*np.pi), (0.0, 0.0, +0.5 * np.pi)
-
+        return (0.0, 0.0, -np.pi / 2), (0.0, 0.0, np.pi / 2)
