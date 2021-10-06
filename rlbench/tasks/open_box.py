4823ac2fe65db1f62a849bba03398c37d642819c
Stephen James <stepjamuk@gmail.com>
2021-10-06 11:54:01 -0700

    Box thickness increased in box tasks.

--- rlbench/tasks/open_box.py
+++ rlbench/tasks/open_box.py
@@ -1,14 +1,15 @@
-from typing import List
+from typing import List, Tuple
+import numpy as np
 from pyrep.objects.joint import Joint
-from rlbench.backend.task import Task
 from rlbench.backend.conditions import JointCondition
+from rlbench.backend.task import Task
 
 
 class OpenBox(Task):
 
     def init_task(self):
-        box_joint = Joint('joint')
-        self.register_success_conditions([JointCondition(box_joint, 1.9)])
+        self.register_success_conditions([
+            JointCondition(Joint('box_joint'), np.pi / 2)])
 
     def init_episode(self, index: int) -> List[str]:
         return ['open box',
@@ -18,3 +19,6 @@ class OpenBox(Task):
 
     def variation_count(self):
         return 1
+
+    def base_rotation_bounds(self) -> Tuple[List[float], List[float]]:
+        return [0, 0, -np.pi / 8], [0, 0, np.pi / 8]
