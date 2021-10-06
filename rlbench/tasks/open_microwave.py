50fc085dbd1e96c1e545e3bc1cca65dc93dc76f6
Stephen James <stepjamuk@gmail.com>
2021-10-06 11:12:12 -0700

    Larger microwave in close and open tasks. Reduced rotation to increase demo success.

--- rlbench/tasks/open_microwave.py
+++ rlbench/tasks/open_microwave.py
@@ -1,10 +1,10 @@
 from typing import List, Tuple
 import numpy as np
-from pyrep.objects.shape import Shape
 from pyrep.objects.joint import Joint
 from pyrep.objects.object import Object
-from rlbench.backend.task import Task
+from pyrep.objects.shape import Shape
 from rlbench.backend.conditions import JointCondition
+from rlbench.backend.task import Task
 
 
 class OpenMicrowave(Task):
@@ -22,7 +22,7 @@ class OpenMicrowave(Task):
         return 1
 
     def base_rotation_bounds(self) -> Tuple[List[float], List[float]]:
-        return [0, 0, -3.14 / 4.], [0, 0, 3.14 / 4.]
+        return [0, 0, -np.pi / 8.], [0, 0, np.pi / 8.]
 
     def boundary_root(self) -> Object:
         return Shape('boundary_root')
