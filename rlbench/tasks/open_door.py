d90670fa73fa8e8d703f7b0dd503ed4a9c02ebcc
Stephen James <stepjamuk@gmail.com>
2021-10-13 08:06:38 -0700

    close_door boundary increase.

--- rlbench/tasks/open_door.py
+++ rlbench/tasks/open_door.py
@@ -1,19 +1,19 @@
 from typing import List, Tuple
 import numpy as np
-from pyrep.objects.shape import Shape
 from pyrep.objects.joint import Joint
 from pyrep.objects.object import Object
-from rlbench.backend.task import Task
+from pyrep.objects.shape import Shape
 from rlbench.backend.conditions import JointCondition
+from rlbench.backend.task import Task
 
 
 class OpenDoor(Task):
 
     def init_task(self) -> None:
         self._door_joint = Joint('door_frame_joint')
-        self.register_success_conditions(
-            [JointCondition(self._door_joint, np.deg2rad(25))])
-        self.door_unlock_cond = JointCondition(
+        self.register_success_conditions([
+            JointCondition(self._door_joint, np.deg2rad(25))])
+        self._door_unlock_cond = JointCondition(
             Joint('door_handle_joint'), np.deg2rad(25))
 
     def init_episode(self, index: int) -> List[str]:
@@ -28,12 +28,12 @@ class OpenDoor(Task):
 
     def step(self) -> None:
         if not self._door_unlocked:
-            self._door_unlocked = self.door_unlock_cond.condition_met()[0]
+            self._door_unlocked = self._door_unlock_cond.condition_met()[0]
             if self._door_unlocked:
                 self._door_joint.set_motor_locked_at_zero_velocity(False)
 
     def base_rotation_bounds(self) -> Tuple[List[float], List[float]]:
-        return [0, 0, -3.14 / 4.], [0, 0, 3.14 / 4.]
+        return [0, 0, -np.pi / 4.], [0, 0, np.pi / 4.]
 
     def boundary_root(self) -> Object:
         return Shape('boundary_root')
