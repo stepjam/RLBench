383685b165bed11b29f882727f9365e6ec52fd00
Stephen James <stepjamuk@gmail.com>
2021-10-15 10:00:08 -0700

    Refactor of close/open grill.

--- rlbench/tasks/close_grill.py
+++ rlbench/tasks/close_grill.py
@@ -1,20 +1,19 @@
 from typing import List, Tuple
-from pyrep.objects.shape import Shape
+
+import numpy as np
+from pyrep.objects.joint import Joint
 from pyrep.objects.object import Object
-from pyrep.objects.proximity_sensor import ProximitySensor
+from pyrep.objects.shape import Shape
+
+from rlbench.backend.conditions import JointCondition
 from rlbench.backend.task import Task
-from rlbench.backend.conditions import DetectedCondition
 
 
 class CloseGrill(Task):
 
     def init_task(self) -> None:
-        self.lid = Shape('lid')
         self.register_success_conditions([
-            DetectedCondition(self.lid, ProximitySensor('sensor_handle'),
-                              negated=True),
-            DetectedCondition(self.lid, ProximitySensor('sensor_grill_top'))
-             ])
+            JointCondition(Joint('lid_joint'), np.deg2rad(50))])
 
     def init_episode(self, index: int) -> List[str]:
         return ['close the grill',
@@ -27,7 +26,7 @@ class CloseGrill(Task):
 
     def base_rotation_bounds(self) -> Tuple[Tuple[float, float, float],
                                             Tuple[float, float, float]]:
-        return (0.0, 0.0, -3.14/2), (0.0, 0.0, 3.14/2)
+        return (0.0, 0.0, -np.pi / 2), (0.0, 0.0, np.pi / 2)
 
     def boundary_root(self) -> Object:
         return Shape('grill_root')
