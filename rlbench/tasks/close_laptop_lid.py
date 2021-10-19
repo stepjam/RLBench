14bd016830143511990f01fa8305ba059d4f0a60
Stephen James <stepjamuk@gmail.com>
2021-10-18 17:30:36 -0700

    close_laptop refactor.

--- rlbench/tasks/close_laptop_lid.py
+++ rlbench/tasks/close_laptop_lid.py
@@ -1,15 +1,13 @@
-from typing import List, Tuple
-import numpy as np
+from typing import List
 from pyrep.objects.joint import Joint
-from rlbench.backend.task import Task
 from rlbench.backend.conditions import JointCondition
+from rlbench.backend.task import Task
 
 
 class CloseLaptopLid(Task):
 
     def init_task(self) -> None:
-        laptop_joint = Joint('joint')
-        self.register_success_conditions([JointCondition(laptop_joint, 1.79)])
+        self.register_success_conditions([JointCondition(Joint('joint'), 1.79)])
 
     def init_episode(self, index: int) -> List[str]:
         return ['close laptop lid',
@@ -18,6 +16,3 @@ class CloseLaptopLid(Task):
 
     def variation_count(self) -> int:
         return 1
-
-    def base_rotation_bounds(self) -> Tuple[List[float], List[float]]:
-        return [0.0, 0.0, np.pi], [0.0, 0.0, np.pi]
