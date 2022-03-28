c2bd28cd505e9b2f5127aa7cc095d20694dd91ec
Stephen James <stepjamuk@gmail.com>
2022-03-28 11:04:12 -0700

    Update setup. Resolves #160.

--- setup.py
+++ setup.py
@@ -34,6 +34,7 @@ setup(name='rlbench',
       packages=[
             'rlbench',
             'rlbench.backend',
+            'rlbench.action_modes',
             'rlbench.tasks',
             'rlbench.task_ttms',
             'rlbench.robot_ttms',
